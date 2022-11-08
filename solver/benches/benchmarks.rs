1eb0eccffe2867fc0aba40f71384d0b4a9ce2e52
Oliver Herrmann <o.herrmann92@gmail.com>
2022-11-08 09:40:59 +0100

    Improve solver benchmarks
    Use custom iteration function to measure average time for finding _one
    solution_, regardless of how many different solvers (w/ different seeds)
    are executed per iteration.

--- solver/benches/benchmarks.rs
+++ solver/benches/benchmarks.rs
@@ -1,10 +1,10 @@
-use std::{cell::RefCell, rc::Rc};
-
 use criterion::{criterion_group, criterion_main, Criterion};
 use model::{map::Map, object::Object, task::Task};
 use rand::{rngs::StdRng, SeedableRng};
 use solver::paths::Paths;
 use solver::solve::Solver;
+use std::time::{Duration, Instant};
+use std::{cell::RefCell, rc::Rc};
 
 const SEEDS: [u64; 3] = [79128620393, 1237923833, 34329582];
 
@@ -13,19 +13,27 @@ macro_rules! run_task {
         let path = concat!(env!("CARGO_MANIFEST_DIR"), "/../", $path);
         let task = Task::from_json_file(path).unwrap();
         let map = Map::from(&task);
-        let mut solvers = (0..3)
-            .map(|i| {
-                let rng = Rc::new(RefCell::new(StdRng::seed_from_u64(SEEDS[i])));
+        let solvers = SEEDS
+            .into_iter()
+            .map(|seed| {
+                let rng = Rc::new(RefCell::new(StdRng::seed_from_u64(seed)));
                 Solver::new(&task, &map, rng)
             })
             .collect::<Vec<Solver<StdRng>>>();
 
-        let mut i = 0;
         $criterion.bench_function(concat!("solve ", $name), move |b| {
-            b.iter(|| {
-                solvers[i].next();
-                i += 1;
-                i %= 3;
+            b.iter_custom(|iterations| {
+                (0..iterations)
+                    .map(|_| {
+                        let solvers = solvers.clone();
+                        let now = Instant::now();
+                        for mut solver in solvers {
+                            solver.next();
+                        }
+                        now.elapsed()
+                    })
+                    .sum::<Duration>()
+                    / solvers.len() as u32
             })
         });
     };
@@ -49,7 +57,7 @@ fn task_004_benchmark(c: &mut Criterion) {
 
 criterion_group! {
     name = solver_benches;
-    config = Criterion::default().significance_level(0.25).sample_size(30);
+    config = Criterion::default().sample_size(50);
     targets = task_001_benchmark, task_002_benchmark, task_003_benchmark, task_004_benchmark,
 }
 
@@ -81,6 +89,33 @@ macro_rules! run_pathfinding {
     };
 }
 
+fn path_finding_task_003(c: &mut Criterion) {
+    run_pathfinding!(
+        c,
+        (20, 20),
+        "inputs/003.task.json",
+        "path finding in task 003"
+    );
+}
+
+fn path_finding_task_004(c: &mut Criterion) {
+    run_pathfinding!(
+        c,
+        (14, 19),
+        "inputs/004.task.json",
+        "path finding in task 004"
+    );
+}
+
+fn path_finding_long_path_001(c: &mut Criterion) {
+    run_pathfinding!(
+        c,
+        (25, 5),
+        "inputs/long_path_001.json",
+        "path finding in long_path 004"
+    );
+}
+
 fn paht_finding_maze_80_80(c: &mut Criterion) {
     run_pathfinding!(
         c,
@@ -92,8 +127,8 @@ fn paht_finding_maze_80_80(c: &mut Criterion) {
 
 criterion_group! {
     name = path_finding_benches;
-    config = Criterion::default().significance_level(0.25).sample_size(100);
-    targets = paht_finding_maze_80_80
+    config = Criterion::default();
+    targets = path_finding_task_003, path_finding_task_004, path_finding_long_path_001, paht_finding_maze_80_80
 }
 
 criterion_main!(solver_benches, path_finding_benches);
